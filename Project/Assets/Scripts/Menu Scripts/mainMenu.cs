389b6db508ea1abd3cf1ea752a2b8732fd1b3887
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2018-11-09 15:06:00 +0000

    Commit 2
    Player Movement
    Camera attached to player

---
+++ Project/Assets/Scripts/Menu Scripts/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainMenu : MonoBehaviour {
+
+    public Button PlayButton;
+    public Button HowToPlay;
+    public Button QuitButton;
+
+    public CanvasGroup MainMenuGroup;
+    public CanvasGroup HowToPlayGroup;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movespeed;
+    public float rotatespeed;
+    public GameObject MoveHelp;
+    private Vector2 MoveDirection;
+    private Vector3 TempMove;
+
+    public Rigidbody2D rigidBody;
+
+    void FixedUpdate()
+    {
+        float moveVector = Input.GetAxis("Vertical");
+        float rotateVector = Input.GetAxis("Horizontal");
+
+        TempMove = MoveHelp.transform.position - this.transform.position;
+        Debug.Log(TempMove);
+
+        MoveDirection = new Vector2(TempMove.x, TempMove.y);
+
+        rigidBody.AddForce(MoveDirection * moveVector * movespeed);
+        //this.transform.Translate(moveVector * movespeed * Time.deltaTime, 0f, 0f);
+        this.transform.Rotate(0f, 0f, -rotateVector * rotatespeed  * Time.deltaTime);
+    }
+}
