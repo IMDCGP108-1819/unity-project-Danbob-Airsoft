2c2980ac75ecba3d83a1196c6ba8a6d6b203efb0
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2018-11-19 17:37:31 +0000

    Commit 3
    Menu Completed
    Ship Selection Cpmpleted
    Fixed Hitbox Collisions
    Set Temp Ship values
    Playable Edge Established
    Player Spawn

---
+++ Project/Assets/Scripts/Menu Scripts/ShipSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShipSelector : MonoBehaviour {
+    public CanvasGroup ShipSelectorGroup;
+    public GameObject StarSkipper;
+    public GameObject PlayerSpawn;
+    public GameObject TX21;
+    public Camera SelectionCamera;
+    private Camera FastCam;
+    public Camera SlowCam;
+
+    private GameObject SpawnedShip;
+
+    public void StarSkipperButton()
+    {
+        SpawnedShip = Instantiate(StarSkipper, PlayerSpawn.transform.position, transform.rotation);
+        ShipSelectorGroup.alpha = 0f;
+        ShipSelectorGroup.interactable = false;
+        ShipSelectorGroup.blocksRaycasts = false;
+        FastCam = SpawnedShip.GetComponentInChildren<Camera>();
+        FastCam.enabled = true;
+        SelectionCamera.enabled = false;
+    }
+
+    public void TX21Button()
+    {
+        SpawnedShip = Instantiate(TX21, PlayerSpawn.transform.position, transform.rotation);
+        ShipSelectorGroup.alpha = 0f;
+        ShipSelectorGroup.interactable = false;
+        ShipSelectorGroup.blocksRaycasts = false;
+        SlowCam = SpawnedShip.GetComponentInChildren<Camera>();
+        SlowCam.enabled = true;
+        SelectionCamera.enabled = false;
+    }
+}
--- Project/Assets/Scripts/Menu Scripts/mainMenu.cs
+++ Project/Assets/Scripts/Menu Scripts/mainMenu.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour {
-
-    public Button PlayButton;
-    public Button HowToPlay;
-    public Button QuitButton;
-
     public CanvasGroup MainMenuGroup;
     public CanvasGroup HowToPlayGroup;
+
+    public void LoadLevel(string SceneToLoad)
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+        public void BackToMenu()
+    {
+        MainMenuGroup.alpha = 1.0f;
+        MainMenuGroup.interactable = true;
+        MainMenuGroup.blocksRaycasts = true;
+
+        HowToPlayGroup.alpha = 0f;
+        HowToPlayGroup.interactable = false;
+        HowToPlayGroup.blocksRaycasts = false;
+    }
+
+    public void HowToPlay()
+    {
+        MainMenuGroup.alpha = 0;
+        MainMenuGroup.interactable = false;
+        MainMenuGroup.blocksRaycasts = false;
+
+        HowToPlayGroup.alpha = 1.0f;
+        HowToPlayGroup.interactable = true;
+        HowToPlayGroup.blocksRaycasts = true;
+    }
 
     public void QuitGame()
     {
