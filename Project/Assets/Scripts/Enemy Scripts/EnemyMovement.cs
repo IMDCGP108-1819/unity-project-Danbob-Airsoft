5971f46f1406b77bd4c1904833f56171d31824b3
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2018-12-03 23:41:22 +0000

    Commit 6
    New Enemy Sprite (Original being saved for possible use with boss later)
    Enemy random movement complete
    Enemy Reaction to world complete- Due to edge collider and one way platform on inner edge enemies do not currently change direction on contact with inside barrier
    Random enemy spawning complete

--- Project/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Project/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -2,11 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
-    //Spawn at random on set spawn locations
+public class EnemyMovement : MonoBehaviour
+{
+    //Spawn at random set spawnpoint
+
 
     //Random Movement
+    public float AccelerationTime;
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 3f;
+    private float CharacterVelocity = 20f;
+    private Vector2 MovementDirection;
+    private Vector2 MovementPerSecond;
+    public GameObject Planet;
 
-    //Change Direction on collision with World Objects and with Outer Border
+    void Start()
+    {
+        latestDirectionChangeTime = 0f;
+        calcuateNewMovementVector();
+    }
 
+    void calcuateNewMovementVector()
+    {
+
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        MovementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        MovementPerSecond = MovementDirection * CharacterVelocity;
+    }
+
+    void Update()
+    {
+        //if the changeTime was reached, calculate a new movement vector
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
+            latestDirectionChangeTime = Time.time;
+            calcuateNewMovementVector();
+        }
+
+        //move enemy: 
+        transform.position = new Vector2(transform.position.x + (MovementPerSecond.x * Time.deltaTime),
+        transform.position.y + (MovementPerSecond.y * Time.deltaTime));
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "World")
+        {
+            calcuateNewMovementVector();
+        }
+
+        if (collision.gameObject.tag == "Outer Barrier")
+        {
+            MovementDirection = -MovementDirection;
+            MovementPerSecond = MovementDirection * CharacterVelocity;
+            StartCoroutine(WallBounce());
+        }
+    }
+    private IEnumerator WallBounce()
+    {
+        yield return new WaitForSeconds(2);
+        calcuateNewMovementVector();
+    }
 }
