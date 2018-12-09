6fef890ee6201c5dcec168a2925d2547a0a9477f
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2018-12-09 11:40:59 +0000

    Commit 7
    Player Health Set
    Player Health Reduction on contact with enemy
    Game Over Screen
    Return to Menu on Game Over

---
+++ Project/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int Health;
+    private Camera GameOverCamera;
+    private CanvasGroup GameOverCanvas;
+    private Camera PlayerCam;
+    private GameObject Spawner;
+
+    public void Start()
+    {
+        GameOverCamera = GameObject.Find("GameOverCamera").GetComponent<Camera>();
+        GameOverCanvas = GameObject.Find("GameOverCanvas").GetComponent<CanvasGroup>();
+        Spawner = GameObject.Find("Enemy Spawn Controller");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Health -= 1;
+            Destroy(collision.gameObject);
+            if (Health == 0)
+            {
+                PlayerCam = this.GetComponentInChildren<Camera>();
+                PlayerCam.enabled = false;
+                GameOverCanvas.alpha = 1f;
+                GameOverCamera.enabled = true;
+                Destroy(this.gameObject);
+                Destroy(Spawner.gameObject);
+                Debug.Log("Ship Destroyed");
+                GameOverCanvas.interactable = true;
+                GameOverCanvas.blocksRaycasts = true;
+            }
+        }
+    }
+}
